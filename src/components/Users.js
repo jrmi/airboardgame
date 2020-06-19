75fed10ed2fe6f28be8902e8f65cdb8e851d2d90
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-06-19 15:21:15 +0200

    Style user list

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,24 +1,35 @@
 import React from "react";
 import UserConfig from "./UserConfig";
 
+import styled from "styled-components";
+
+const UserList = styled.ul`
+  position: fixed;
+  right: 1em;
+  top: 1em;
+  background: #ffffff77;
+  padding: 0.2em;
+  list-style: none;
+`;
+
+const UserListItem = styled.li`
+  display: flex;
+  position: relative;
+`;
+
+const UserListIndex = styled.span`
+  line-height: 30px;
+`;
+
 export const Users = ({ user, setUser, users }) => (
-  <ul
-    style={{
-      position: "fixed",
-      right: "1em",
-      top: "1em",
-      background: "#ffffff77",
-      padding: "0.2em",
-      listStyle: "none",
-    }}
-  >
+  <UserList>
     {users.map((u, index) => (
-      <li key={u.id} style={{ display: "flex", position: "relative" }}>
-        <span style={{ lineHeight: "30px" }}>{index} - </span>
+      <UserListItem key={u.id}>
+        <UserListIndex>{index} - </UserListIndex>
         <UserConfig user={u} setUser={setUser} editable={user.id === u.id} />
-      </li>
+      </UserListItem>
     ))}
-  </ul>
+  </UserList>
 );
 
 export default Users;
