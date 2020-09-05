3ac03488c01a58ba1da68e32f8a4bbbb441f6de8
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-09-05 21:38:03 +0200

    Use persistent database backend

--- src/server/store.js
+++ src/server/store.js
@@ -1,7 +1,15 @@
-import { nanoid } from "nanoid";
 import express from "express";
+import { memoryBackend } from "./storeBackends.js";
 
 // Utility functions
+
+// ROADMAP
+// - Add bulk operations with atomicity
+// - Handle more permissions types (Read, Delete, …)
+// - Allow to send encrypted code block
+// - Add Queries
+// - Add relationship
+// - Add http2 relationship ?
 
 const throwError = (message, code = 400) => {
   const errorObject = new Error(message);
@@ -9,153 +17,23 @@ const throwError = (message, code = 400) => {
   throw errorObject;
 };
 
-const errorGuard = (func) => (req, res, next) => {
+const errorGuard = (func) => async (req, res, next) => {
   try {
-    return func(req, res, next);
+    return await func(req, res, next);
   } catch (error) {
     console.log(error);
     next(error);
   }
 };
 
-// Memory backend for proof of concept
-export const memoryBackend = () => {
-  const data = {};
-  const security = {};
-
-  const getOrCreateBox = (boxId) => {
-    if (typeof data[boxId] !== "object") {
-      data[boxId] = {};
-    }
-    return data[boxId];
-  };
-
-  const filterObjectProperties = (obj, propArr) => {
-    const newObj = {};
-    for (let key in obj) {
-      if (propArr.includes(key)) {
-        newObj[key] = obj[key];
-      }
-    }
-    return newObj;
-  };
-
-  return {
-    checkSecurity(boxId, id, key) {
-      if (!security[boxId]) {
-        // Not secured
-        if (key) {
-          // Set security
-          security[boxId] = {};
-
-          if (id) {
-            // Id -> it's ressource security
-            security[boxId][id] = key;
-          } else {
-            // No id -> it's box security
-            security[boxId]._box = key;
-          }
-        }
-        return true;
-      }
-
-      if (id) {
-        if (security[boxId][id] === undefined) {
-          security[boxId][id] = key;
-          return true;
-        }
-        return security[boxId][id] === key;
-      } else {
-        return (
-          security[boxId]._box !== undefined && security[boxId]._box === id
-        );
-      }
-    },
-    list(boxId, { limit = 50, sort, skip = 0, onlyFields = [], q }) {
-      if (data[boxId] === undefined) {
-        return [];
-      }
-      let result = Object.values(data[boxId]);
-
-      let sortProperty = sort;
-      let asc = true;
-
-      // If prefixed with '-' inverse order
-      if (sort[0] === "-") {
-        sortProperty = sort.substring(1);
-        asc = false;
-      }
-
-      result.sort((resource1, resource2) => {
-        if (resource1[sortProperty] < resource2[sortProperty]) {
-          return asc ? -1 : 1;
-        }
-        if (resource1[sortProperty] > resource2[sortProperty]) {
-          return asc ? 1 : -1;
-        }
-        return 0;
-      });
-
-      result = result.slice(skip, skip + limit);
-
-      if (onlyFields.length) {
-        result = result.map((resource) =>
-          filterObjectProperties(resource, onlyFields)
-        );
-      }
-      return result;
-    },
-    get(boxId, id) {
-      if (data[boxId] === undefined) {
-        throwError("Box not found", 404);
-      }
-      if (!data[boxId][id]) {
-        throwError("Ressource not found", 404);
-      }
-      return data[boxId][id];
-    },
-    create(boxId, data) {
-      const newRessource = { ...data, _id: nanoid(), _createdOn: new Date() };
-      getOrCreateBox(boxId)[newRessource._id] = newRessource;
-      return newRessource;
-    },
-    update(boxId, id, body) {
-      //console.log(boxId, id, body);
-      // To prevent created modification
-      if (!data[boxId]) {
-        throwError("Box not found", 404);
-      }
-      if (!data[boxId][id]) {
-        throwError("Ressource not found", 404);
-      }
-      const { created } = data[boxId][id];
-      const updatedItem = {
-        ...data[boxId][id],
-        ...body,
-        _id: id,
-        created,
-        _updatedOn: new Date(),
-      };
-      data[boxId][id] = updatedItem;
-      return updatedItem;
-    },
-    delete(boxId, id) {
-      if (data[boxId][id] !== undefined) {
-        delete data[boxId][id];
-        return 1;
-      }
-      return 0;
-    },
-  };
-};
-
 // Store Middleware
-export const store = (prefix = "/store", backend = memoryBackend()) => {
+export const store = ({ prefix = "/store", backend = memoryBackend() }) => {
   const router = express.Router();
-  // Object list
+
+  // Resource list
   router.get(
     `${prefix}/:boxId/`,
-    errorGuard((req, res) => {
+    errorGuard(async (req, res) => {
       const { boxId } = req.params;
       const {
         limit = "50",
@@ -170,8 +48,18 @@ export const store = (prefix = "/store", backend = memoryBackend()) => {
       const parsedLimit = parseInt(limit, 10);
       const parsedSkip = parseInt(skip, 10);
 
-      const result = backend.list(boxId, {
-        sort,
+      let sortProperty = sort;
+      let asc = true;
+
+      // If prefixed with '-' inverse order
+      if (sort[0] === "-") {
+        sortProperty = sort.substring(1);
+        asc = false;
+      }
+
+      const result = await backend.list(boxId, {
+        sort: sortProperty,
+        asc,
         limit: parsedLimit,
         skip: parsedSkip,
         onlyFields: onlyFields,
@@ -184,10 +72,10 @@ export const store = (prefix = "/store", backend = memoryBackend()) => {
   // One object
   router.get(
     `${prefix}/:boxId/:id`,
-    errorGuard((req, res) => {
+    errorGuard(async (req, res) => {
       const { boxId, id } = req.params;
-      const result = backend.get(boxId, id);
-      if (result === undefined) {
+      const result = await backend.get(boxId, id);
+      if (!result) {
         throwError("Box or ressource not found", 404);
       }
       res.json(result);
@@ -197,14 +85,14 @@ export const store = (prefix = "/store", backend = memoryBackend()) => {
   // Create object
   router.post(
     `${prefix}/:boxId/`,
-    errorGuard((req, res) => {
+    errorGuard(async (req, res) => {
       //console.log(req.body);
       const { boxId } = req.params;
       /*const { key } = req.query;
       if (!backend.checkSecurity(boxId, null, key)) {
         throwError("You need write access for this box", 403);
       }*/
-      const result = backend.create(boxId, req.body);
+      const result = await backend.create(boxId, req.body);
       return res.json(result);
     })
   );
@@ -212,13 +100,13 @@ export const store = (prefix = "/store", backend = memoryBackend()) => {
   // Update object
   router.put(
     `${prefix}/:boxId/:id`,
-    errorGuard((req, res) => {
+    errorGuard(async (req, res) => {
       const { boxId, id } = req.params;
       const { key } = req.query;
-      if (!backend.checkSecurity(boxId, id, key)) {
+      if (!(await backend.checkSecurity(boxId, id, key))) {
         throwError("You need write access for this ressource", 403);
       }
-      const result = backend.update(boxId, id, req.body);
+      const result = await backend.update(boxId, id, req.body);
       return res.json(result);
     })
   );
@@ -226,13 +114,13 @@ export const store = (prefix = "/store", backend = memoryBackend()) => {
   // Delete object
   router.delete(
     `${prefix}/:boxId/:id`,
-    errorGuard((req, res) => {
+    errorGuard(async (req, res) => {
       const { boxId, id } = req.params;
       const { key } = req.query;
-      if (!backend.checkSecurity(boxId, id, key)) {
+      if (!(await backend.checkSecurity(boxId, id, key))) {
         throwError("You need write access for this ressource", 403);
       }
-      const result = backend.delete(boxId, id);
+      const result = await backend.delete(boxId, id);
       if (result === 1) {
         res.json({ message: "Deleted" });
       }
---
+++ src/server/storeBackends.js
@@ -0,0 +1,286 @@
+import { nanoid } from "nanoid";
+import Datastore from "nedb";
+
+const throwError = (message, code = 400) => {
+  const errorObject = new Error(message);
+  errorObject.statusCode = code;
+  throw errorObject;
+};
+
+// Memory backend for proof of concept
+export const memoryBackend = () => {
+  const data = {};
+  const security = {};
+
+  const getOrCreateBox = (boxId) => {
+    if (typeof data[boxId] !== "object") {
+      data[boxId] = {};
+    }
+    return data[boxId];
+  };
+
+  const filterObjectProperties = (obj, propArr) => {
+    const newObj = {};
+    for (let key in obj) {
+      if (propArr.includes(key)) {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+  };
+
+  return {
+    async checkSecurity(boxId, id, key) {
+      if (!security[boxId]) {
+        // Not secured
+        if (key) {
+          // Set security
+          security[boxId] = {};
+
+          if (id) {
+            // Id -> it's ressource security
+            security[boxId][id] = key;
+          } else {
+            // No id -> it's box security
+            security[boxId]._box = key;
+          }
+        }
+        return true;
+      }
+
+      if (id) {
+        if (security[boxId][id] === undefined) {
+          security[boxId][id] = key;
+          return true;
+        }
+        return security[boxId][id] === key;
+      } else {
+        return (
+          security[boxId]._box !== undefined && security[boxId]._box === id
+        );
+      }
+    },
+    async list(boxId, { limit, sort, asc, skip, onlyFields, q }) {
+      if (data[boxId] === undefined) {
+        return [];
+      }
+      let result = Object.values(data[boxId]);
+
+      result.sort((resource1, resource2) => {
+        if (resource1[sort] < resource2[sort]) {
+          return asc ? -1 : 1;
+        }
+        if (resource1[sort] > resource2[sort]) {
+          return asc ? 1 : -1;
+        }
+        return 0;
+      });
+
+      result = result.slice(skip, skip + limit);
+
+      if (onlyFields.length) {
+        result = result.map((resource) =>
+          filterObjectProperties(resource, onlyFields)
+        );
+      }
+      return result;
+    },
+    async get(boxId, id) {
+      if (data[boxId] === undefined) {
+        return null;
+      }
+      if (!data[boxId][id]) {
+        return null;
+      }
+      return data[boxId][id];
+    },
+    async create(boxId, data) {
+      const newRessource = { ...data, _id: nanoid(), _createdOn: new Date() };
+      getOrCreateBox(boxId)[newRessource._id] = newRessource;
+      return newRessource;
+    },
+    async update(boxId, id, body) {
+      // To prevent created modification
+      if (!data[boxId]) {
+        throwError("Box not found", 404);
+      }
+      if (!data[boxId][id]) {
+        throwError("Ressource not found", 404);
+      }
+      const { created } = data[boxId][id];
+      const updatedItem = {
+        ...data[boxId][id],
+        ...body,
+        _id: id,
+        created,
+        _updatedOn: new Date(),
+      };
+      data[boxId][id] = updatedItem;
+      return updatedItem;
+    },
+    async delete(boxId, id) {
+      if (data[boxId][id] !== undefined) {
+        delete data[boxId][id];
+        return 1;
+      }
+      return 0;
+    },
+  };
+};
+
+// Nedb backend for proof of concept
+export const NeDBBackend = (options) => {
+  const db = {};
+  db.boxes = new Datastore({
+    ...options,
+    filename: `${options.dirname}/boxes.json`,
+    autoload: true,
+  });
+
+  const createBoxRecord = (boxId) => {
+    return new Promise((resolve, reject) => {
+      db.boxes.insert({ box: boxId }, (err, doc) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(doc);
+      });
+    });
+  };
+
+  const getBoxRecord = (boxId) => {
+    return new Promise((resolve, reject) => {
+      db.boxes.findOne({ box: boxId }, (err, doc) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(doc);
+      });
+    });
+  };
+
+  const getBoxDB = (boxId) => {
+    if (!db[boxId]) {
+      db[boxId] = new Datastore({
+        ...options,
+        filename: `${options.dirname}/${boxId}.json`,
+        autoload: true,
+      });
+    }
+    return db[boxId];
+  };
+
+  return {
+    async checkSecurity(boxId, id, key) {
+      return true;
+    },
+    async list(boxId, { limit, sort, asc, skip, onlyFields, q }) {
+      const boxRecord = await getBoxRecord(boxId);
+      if (!boxRecord) {
+        return [];
+      }
+      const boxDB = getBoxDB(boxId);
+      return new Promise((resolve, reject) => {
+        boxDB
+          .find(
+            {},
+            onlyFields.length
+              ? onlyFields.reduce((acc, field) => {
+                  acc[field] = 1;
+                  return acc;
+                }, {})
+              : {}
+          )
+          .limit(limit)
+          .skip(skip)
+          .sort({ [sort]: asc ? 1 : -1 })
+          .exec((err, docs) => {
+            if (err) {
+              reject(err);
+            }
+            resolve(docs);
+          });
+      });
+    },
+    async get(boxId, id) {
+      const boxRecord = await getBoxRecord(boxId);
+      if (!boxRecord) {
+        return [];
+      }
+      const boxDB = getBoxDB(boxId);
+      return new Promise((resolve, reject) => {
+        boxDB.findOne({ _id: id }, (err, doc) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(doc);
+        });
+      });
+    },
+    async create(boxId, data) {
+      const boxRecord = await getBoxRecord(boxId);
+      if (!boxRecord) {
+        createBoxRecord(boxId);
+      }
+      const boxDB = getBoxDB(boxId);
+      return new Promise((resolve, reject) => {
+        boxDB.insert(data, (err, doc) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(doc);
+        });
+      });
+    },
+    async update(boxId, id, body) {
+      const boxRecord = await getBoxRecord(boxId);
+      if (!boxRecord) {
+        throwError("Box not found", 404);
+      }
+      const boxDB = getBoxDB(boxId);
+      return new Promise((resolve, reject) => {
+        boxDB.update(
+          { _id: id },
+          body,
+          { returnUpdatedDocs: true },
+          (err, numAffected, affectedDoc) => {
+            if (!numAffected) {
+              throwError("Ressource not found", 404);
+            }
+            if (err) {
+              reject(err);
+            }
+            resolve(affectedDoc);
+          }
+        );
+      });
+    },
+    async delete(boxId, id) {
+      const boxRecord = await getBoxRecord(boxId);
+      if (!boxRecord) {
+        throwError("Box not found", 404);
+      }
+      const boxDB = getBoxDB(boxId);
+      return new Promise((resolve, reject) => {
+        boxDB.remove({ _id: id }, {}, (err, numRemoved) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(numRemoved);
+        });
+      });
+    },
+  };
+};
+
+// Backend interface
+/*export const Backend = () => {
+  return {
+    async checkSecurity(boxId, id, key) {},
+    async list(boxId, { limit, sort, skip, onlyFields, q }) {},
+    async get(boxId, id) {},
+    async create(boxId, data) {},
+    async update(boxId, id, body) {},
+    async delete(boxId, id) {},
+  };
+};*/
