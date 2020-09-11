a89405938ad0790c6551f83de647147c31a98465
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-09-11 17:28:23 +0200

    Add namespace and namespace listing

--- src/server/fileStorage.js
+++ src/server/fileStorage.js
@@ -5,8 +5,10 @@ import multerS3 from "multer-s3";
 import mime from "mime-types";
 import { nanoid } from "nanoid";
 import express from "express";
+import path from "path";
+import fs from "fs";
 
-export const fileStorage = ({ type, config }) => {
+export const fileStorage = ({ prefix = "/file", type, config }) => {
   const app = express.Router();
 
   app.use(bodyParser.json());
@@ -16,15 +18,46 @@ export const fileStorage = ({ type, config }) => {
     const imageMap = {};
     const upload = multer({ storage: multer.memoryStorage() });
 
-    app.post("/upload", upload.single("file"), (req, res) => {
+    app.post(`${prefix}/:namespace/`, upload.single("file"), (req, res) => {
+      const {
+        params: { namespace },
+      } = req;
+
       const ext = mime.extension(req.file.mimetype);
       const filename = `${nanoid()}.${ext}`;
-      imageMap[filename] = req.file.buffer;
-      res.send(`${config.url}/image/${filename}`);
+      const store = imageMap[namespace] || {};
+      store[filename] = req.file.buffer;
+      imageMap[namespace] = store;
+      res.send(`${config.url}/${prefix}/:namespace/${filename}`);
     });
 
-    app.get("/image/:filename", (req, res) => {
-      const fileBuffer = imageMap[req.params.filename];
+    app.get(`${prefix}/:namespace/`, (req, res) => {
+      const {
+        params: { namespace },
+      } = req;
+
+      const store = imageMap[namespace] || {};
+      const result = Object.keys(store);
+      res.json(result);
+    });
+
+    app.get(`${prefix}/:namespace/:filename`, (req, res) => {
+      const {
+        params: { filename, namespace },
+      } = req;
+
+      if (!imageMap[namespace]) {
+        res.status(404).send("Not found");
+        return;
+      }
+
+      const fileBuffer = imageMap[namespace][filename];
+
+      if (!fileBuffer) {
+        res.status(404).send("Not found");
+        return;
+      }
+
       res.send(fileBuffer);
     });
   }
@@ -32,8 +65,13 @@ export const fileStorage = ({ type, config }) => {
   // File storage
   if (type === "disk") {
     const storage = multer.diskStorage({
-      destination: config.destination,
-      filename: function (req, file, cb) {
+      destination: (req) => {
+        const {
+          params: { namespace = "default" },
+        } = req.params;
+        return path.join(config.destination, namespace);
+      },
+      filename: (req, file, cb) => {
         const ext = mime.extension(file.mimetype);
         const filename = `${nanoid()}.${ext}`;
         cb(null, filename);
@@ -42,12 +80,32 @@ export const fileStorage = ({ type, config }) => {
 
     const upload = multer({ storage: storage });
 
-    app.post("/upload", upload.single("file"), (req, res) => {
-      res.send(`${config.url}/image/${req.file.filename}`);
+    app.post(`${prefix}/:namespace/`, upload.single("file"), (req, res) => {
+      res.send(`${config.url}/${prefix}/file/${req.file.filename}`);
     });
 
-    app.get("/image/:filename", (req, res) => {
-      const file = `${config.destination}/${req.params.filename}`;
+    app.get(`${prefix}/:namespace/`, (req, res) => {
+      const {
+        params: { namespace },
+      } = req;
+
+      const dir = path.join(config.destination, namespace);
+
+      fs.readdir(dir, (err, files) => {
+        if (err) {
+          res.json([]);
+        } else {
+          res.json(files);
+        }
+      });
+    });
+
+    app.get(`${prefix}/:namespace/:filename`, (req, res) => {
+      const {
+        params: { filename, namespace },
+      } = req;
+
+      const file = path.join(config.destination, namespace, filename);
       res.download(file);
     });
   }
@@ -69,15 +127,39 @@ export const fileStorage = ({ type, config }) => {
         bucket: config.bucket,
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: (req, file, cb) => {
+          const {
+            params: { namespace },
+          } = req;
+
           const ext = mime.extension(file.mimetype);
-          cb(null, `${nanoid()}.${ext}`);
+          cb(null, `${namespace}/${nanoid()}.${ext}`);
         },
       }),
       limits: { fileSize: 1024 * 1024 * 5 }, // 5MB
     });
 
-    app.post("/upload", upload.single("file"), (req, res) => {
+    app.post(`${prefix}/:namespace/`, upload.single("file"), (req, res) => {
       res.send(req.file.location);
+    });
+
+    app.get(`${prefix}/:namespace/`, (req, res) => {
+      const {
+        params: { namespace },
+      } = req;
+
+      const params = {
+        Bucket: config.bucket,
+        Delimiter: "/",
+        Prefix: `/${namespace}/`,
+      };
+
+      s3.listObjects(params, (err, data) => {
+        if (err) {
+          return [];
+        }
+        console.log(data);
+        res.send(data);
+      });
     });
   }
   return app;
