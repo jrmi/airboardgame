600baec93453fa843efdb4f64b0a5aaf532e3c54
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-09-25 17:04:30 +0200

    Better home page v2

--- src/views/GamesView.js
+++ src/views/GamesView.js
@@ -11,13 +11,13 @@ import "react-confirm-alert/src/react-confirm-alert.css";
 import styled from "styled-components";
 
 const GameView = styled.ul`
-  width: 50%;
+  /*width: 50%;*/
   margin: 0 auto;
   position: relative;
   & .new-game {
     position: absolute;
     top: 1em;
-    right: 2px;
+    right: 1em;
   }
   & h1 {
     color: hsl(210, 14%, 75%);
@@ -28,16 +28,23 @@ const GameList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
 `;
 
 const Game = styled.li`
   width: 100%;
   background-color: hsl(210, 26%, 19%);
   color: hsl(210, 14%, 75%);
-  display: flex;
-  justify-content: space-between;
-
-  & .game-name {
+  position: relative;
+  min-width: 300px;
+  max-width: 350px;
+  height: 150px;
+  padding: 1em;
+  margin: 1em;
+  flex: 1 1 0%;
+  width: & .game-name {
     margin: 0 1em;
   }
 
@@ -45,8 +52,21 @@ const Game = styled.li`
     margin: 0 2px;
   }
 
-  & .action {
-    padding: 0.6em 0;
+  & .play {
+    position: absolute;
+    bottom: 0.5em;
+    right: 0.5em;
+  }
+
+  & .extra-actions {
+    position: absolute;
+    top: 0.5em;
+    right: 0.5em;
+    display: none;
+  }
+
+  &:hover .extra-actions {
+    display: block;
   }
 `;
 
@@ -87,21 +107,24 @@ const GamesView = () => {
   return (
     <GameView>
       <Link to={`/game/`} className="button new-game">
-        Create new game
+        {t("Create new game")}
       </Link>
-      <h1>Game list</h1>
+      <h1>{t("Game list")}</h1>
       <GameList>
         {gameList.map(({ name, id }) => (
           <Game key={id}>
             <h2 className="game-name">{name}</h2>
-            <div className="action">
-              <Link to={`/game/${id}/session/`} className="button success">
-                play
+            <Link to={`/game/${id}/session/`} className="button success play">
+              {t("Play")}
+            </Link>
+            <div className="extra-actions">
+              <Link to={`/game/${id}/edit`} className="button edit">
+                {t("Edit")}
               </Link>
-              <Link to={`/game/${id}/edit`} className="button">
-                edit
-              </Link>
-              <button onClick={handleRemove(id)} className="button error">
+              <button
+                onClick={handleRemove(id)}
+                className="button error delete"
+              >
                 X
               </button>
             </div>
