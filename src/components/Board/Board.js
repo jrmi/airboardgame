184db85bdcedd7df4fccd642649918cec2e65ec1
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-10-30 11:13:32 +0100

    Enhance board style

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -24,6 +24,10 @@ const StyledBoard = styled.div.attrs(() => ({ className: "board" }))`
   background-image: url(/board.png);
   width: ${({ size }) => size}px;
   height: ${({ size }) => size}px;
+  border-radius: 1em;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px,
+    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;*
 `;
 
 export const Board = ({ user, users, getComponent }) => {
