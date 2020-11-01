cd474459cdb7d45364d482ba058188509146c61f
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-11-01 21:09:50 +0100

    Enhance some board component style

--- src/components/boardComponents/Rect.js
+++ src/components/boardComponents/Rect.js
@@ -6,6 +6,8 @@ const Rect = styled.div`
     width: ${width}px;
     height: ${height}px;
     background-color: ${color};
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   `}
 `;
 
--- src/components/boardComponents/Round.js
+++ src/components/boardComponents/Round.js
@@ -11,6 +11,8 @@ const StyledRound = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
   `}
 `;
 
