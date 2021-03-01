2577fa619fc14e6cca436f39e7ecb468101dae88
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-03-01 22:25:38 +0100

    Fix dark mode effect

--- src/components/boardComponents/Rect.js
+++ src/components/boardComponents/Rect.js
@@ -1,14 +1,60 @@
-import { memo } from "react";
+import React, { memo } from "react";
 import styled, { css } from "styled-components";
 
-const Rect = styled.div`
-  ${({ width = 50, height = 50, color = "#ccc" }) => css`
+const StyledShape = styled.div`
+  ${({ width, height }) => css`
     width: ${width}px;
     height: ${height}px;
-    background-color: ${color};
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
       rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    & svg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+    & span {
+      z-index: 0;
+    }
   `}
 `;
+
+const Rect = ({
+  width = 50,
+  height = 50,
+  color = "#ccc",
+  text = "",
+  textColor = "#000",
+  fontSize = "16",
+}) => {
+  return (
+    <StyledShape width={width} height={height}>
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <rect
+          x="0"
+          y="0"
+          width="100"
+          height="100"
+          style={{
+            fill: color,
+          }}
+        />
+      </svg>
+      <span
+        style={{
+          textColor,
+          fontSize: fontSize + "px",
+        }}
+      >
+        {text}
+      </span>
+    </StyledShape>
+  );
+};
 
 export default memo(Rect);
--- src/components/boardComponents/Round.js
+++ src/components/boardComponents/Round.js
@@ -2,33 +2,56 @@ import React, { memo } from "react";
 import styled, { css } from "styled-components";
 
 const StyledRound = styled.div`
-  ${({ radius = 50, color = "#ccc" }) => css`
+  ${({ radius = 50 }) => css`
     border-radius: 100%;
     width: ${radius}px;
     height: ${radius}px;
-    background-color: ${color};
     text-align: center;
     display: flex;
     align-items: center;
     justify-content: center;
     box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
       rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    & svg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+    & span {
+      z-index: 0;
+    }
   `}
 `;
 
 const Round = ({
   size,
   radius = size,
-  color,
+  color = "#ccc",
   text = "",
   textColor = "#000",
   fontSize = "16",
 }) => {
   return (
-    <StyledRound radius={radius} color={color}>
+    <StyledRound radius={radius}>
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle
+          cx="50"
+          cy="50"
+          r="50"
+          style={{
+            fill: color,
+          }}
+        />
+      </svg>
       <span
         style={{
-          textColor,
+          color: textColor,
           fontSize: fontSize + "px",
         }}
       >
