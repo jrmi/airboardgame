9368672101c1067564a1e3172390699fa292d681
Ribakys <79149378+Ribakys@users.noreply.github.com>
2021-03-17 00:31:10 +0100

    Update src/components/boardComponents/index.js

--- src/components/boardComponents/index.js
+++ src/components/boardComponents/index.js
@@ -102,8 +102,6 @@ export const itemMap = {
           "flipSelf",
           "tap",
           "stack",
-          "alignAsLine",
-          "alignAsSquare",
           "shuffle",
           "clone",
           "lock",
