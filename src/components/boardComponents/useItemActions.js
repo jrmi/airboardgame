1e9ead241d2ceb06ae3893a254e169936934ded7
Ribakys <79149378+Ribakys@users.noreply.github.com>
2021-03-16 23:37:10 +0100

    Update src/components/boardComponents/useItemActions.js

--- src/components/boardComponents/useItemActions.js
+++ src/components/boardComponents/useItemActions.js
@@ -182,7 +182,7 @@ export const useItemActions = () => {
       );
       minMax.max.y = Math.max(
         ...selectedItemList.map(
-          ({ y, id }) => y //+ document.getElementById(id).clientHeight
+          ({ y, id }) => y
         )
       );
 
