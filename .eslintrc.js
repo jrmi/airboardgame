686f693d099452f8578af061dc65333ab6fd434e
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-14 21:29:39 +0200

    Configure linting for switch

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   plugins: ["react", "react-hooks"],
   rules: {
-    indent: ["error", 2],
+    indent: ["error", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
