b0d36a809ea5e072be1756a605fb9b6aeba27d63
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-21 21:27:24 +0200

    Add auth tests

---
+++ cypress/integration/authentication.spec.js
@@ -0,0 +1,57 @@
+describe("Studio", () => {
+  beforeEach(() => {
+    cy.viewport(1000, 600);
+    cy.visit("/");
+  });
+
+  it("Can see Log in button", () => {
+    cy.contains("Log in");
+  });
+
+  it("Can ask to log in", () => {
+    cy.intercept(
+      {
+        method: "POST",
+        url: "/airboardgame/auth/",
+      },
+      (req) => {
+        expect(req.body).to.deep.equal({ userEmail: "test@fake.fr" });
+        req.reply(req.body);
+      }
+    );
+
+    cy.contains("Log in").click();
+    cy.get(".side-panel input").type("test@fake.fr");
+
+    cy.contains("Ask an authentication link").click();
+
+    cy.contains("Mail sent");
+    cy.contains("Ok").click();
+
+    cy.contains("Mail sent").should("not.exist");
+  });
+
+  it.only("Can verify login", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/airboardgame/auth/verify/titi/toto",
+      },
+      (req) => {
+        req.reply("Ok");
+      }
+    );
+    cy.intercept(
+      {
+        method: "GET",
+        url: "/airboardgame/auth/check",
+      },
+      // eslint-disable-next-line quotes
+      '{"message":"success"}'
+    );
+
+    cy.visit("/login/titi/toto");
+
+    cy.contains("Studio");
+  });
+});
