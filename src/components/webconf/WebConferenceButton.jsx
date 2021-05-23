bf12830f13a0a8df73bae04929390f56e330df70
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-05-23 23:18:59 +0200

    Save webconf in localstorage

--- src/components/webconf/WebConferenceButton.jsx
+++ src/components/webconf/WebConferenceButton.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import Touch from "../../ui/Touch";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 import WebConference from "./WebConference";
 
 export const WeConferenceButton = ({ room }) => {
   const { t } = useTranslation();
-  const [webConference, setWebConference] = React.useState(false);
+  const [webConference, setWebConference] = useLocalStorage(
+    "enableWebconference",
+    false
+  );
 
   return (
     <>
