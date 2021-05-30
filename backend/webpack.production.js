da322d7170fd324702e8fed3461229063186ca0f
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-05-30 16:35:54 +0200

    Update webpack config file after update

--- backend/webpack.production.js
+++ backend/webpack.production.js
@@ -1,6 +1,6 @@
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 
-module.exports = merge.smart(common, {
+module.exports = merge(common, {
   mode: "production",
 });
