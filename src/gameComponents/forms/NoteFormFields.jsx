1fb7fcb2d4783c3340e65a02605a4f0200760a85
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2022-03-29 22:15:41 +0200

    Add more control to Note item

--- src/gameComponents/forms/NoteFormFields.jsx
+++ src/gameComponents/forms/NoteFormFields.jsx
@@ -22,7 +22,7 @@ const Form = ({ initialValues }) => {
         <Field
           name="width"
           component="input"
-          initialValue={initialValues.width}
+          initialValue={initialValues.width || 300}
         >
           {(props) => <input {...props.input} type="number" />}
         </Field>
@@ -32,7 +32,7 @@ const Form = ({ initialValues }) => {
         <Field
           name="height"
           component="input"
-          initialValue={initialValues.height}
+          initialValue={initialValues.height || 200}
         >
           {(props) => <input {...props.input} type="number" />}
         </Field>
@@ -42,7 +42,41 @@ const Form = ({ initialValues }) => {
         <Field
           name="color"
           component="input"
-          initialValue={initialValues.color}
+          initialValue={initialValues.color || "#ffc"}
+        >
+          {({ input: { onChange, value } }) => (
+            <ColorPicker value={value} onChange={onChange} />
+          )}
+        </Field>
+      </Label>
+      <Label>
+        {t("Font size")}
+        <Field
+          name="fontSize"
+          component="input"
+          initialValue={initialValues.fontSize || 20}
+        >
+          {(props) => <input {...props.input} type="number" />}
+        </Field>
+      </Label>
+      <Label>
+        {t("Font family")}
+        <Field
+          name="fontFamily"
+          component="select"
+          initialValue={initialValues.fontFamily || "Roboto"}
+          style={{ width: "10em" }}
+        >
+          <option value="Roboto">Roboto</option>
+          <option value="Satisfy">Satisfy</option>
+        </Field>
+      </Label>
+      <Label>
+        {t("Text color")}
+        <Field
+          name="textColor"
+          component="input"
+          initialValue={initialValues.textColor || "#000"}
         >
           {({ input: { onChange, value } }) => (
             <ColorPicker value={value} onChange={onChange} />
