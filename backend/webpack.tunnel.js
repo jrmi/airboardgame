ec167c8d2f558564de5c665105d39791ba236fe4
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2022-06-11 15:15:17 +0200

    Fix deps

--- backend/webpack.tunnel.js
+++ backend/webpack.tunnel.js
@@ -1,24 +1,22 @@
-const path = require('path');
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
-const LocaltunnelPlugin = require('localtunnel-webpack-plugin');
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+const LocaltunnelPlugin = require("webpack-plugin-localtunnel");
 
 const prefix =
   process.env.TUNNEL_PREFIX || Math.random().toString().substr(2, 8);
 
 module.exports = merge(common, {
-  mode: 'development',
+  mode: "development",
   watchOptions: {
-    ignored: ['node_modules/**'],
+    ignored: ["node_modules/**"],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    public: prefix + '-ricochet.loca.lt',
+    contentBase: path.join(__dirname, "dist"),
+    public: prefix + "-ricochet.loca.lt",
     compress: true,
-    allowedHosts: ['.loca.lt'],
+    allowedHosts: [".loca.lt"],
     port: 9000,
   },
-  plugins: [
-    new LocaltunnelPlugin({ localtunnel: { subdomain: prefix + '-ricochet' } }),
-  ],
+  plugins: [new LocaltunnelPlugin({ subdomain: prefix + "-ricochet" })],
 });
