e8c13e07ac587c7d3fe43b1a04b0d2fb800a46cf
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2022-06-11 15:28:37 +0200

    Fix tests

--- cypress/integration/item.spec.js
+++ cypress/integration/item.spec.js
@@ -41,7 +41,7 @@ describe("Item interactions", () => {
     cy.get("img[src='/game_assets/JC.jpg']")
       .parents(".item")
       .trigger("pointerdown", {
-        button: 0,
+        buttons: 1,
         clientX: 430,
         clientY: 430,
         force: true,
@@ -51,7 +51,7 @@ describe("Item interactions", () => {
     cy.get("img[src='/game_assets/JC.jpg']")
       .parents(".item")
       .trigger("pointermove", {
-        button: 0,
+        buttons: 1,
         clientX: 530,
         clientY: 530,
         force: true,
