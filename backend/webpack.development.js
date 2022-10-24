8139c9c54d4dd6187b189cbe95b792e98d777fee
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2022-10-24 23:16:48 +0200

    Switch to railway

--- backend/webpack.development.js
+++ backend/webpack.development.js
@@ -1,6 +1,9 @@
 import path from "path";
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
+import { fileURLToPath } from "url";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default merge(common, {
   mode: "development",
@@ -8,7 +11,7 @@ export default merge(common, {
     ignored: ["node_modules/**"],
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(dirname, "dist"),
     compress: true,
     port: 9000,
   },
